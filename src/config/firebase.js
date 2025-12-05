b6768ad32ac7b962543e1149a2e0303142ae95a1
andreeaionescu26 <andreea26.ionescu@gmail.com>
2025-12-05 14:22:49 +0000

    updated firebase config to sue env variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore';;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBl5sWc_QP5lsmLkYBmxeFyyY1NtExgw14",
-  authDomain: "wedding-website-2026-a9de1.firebaseapp.com",
-  projectId: "wedding-website-2026-a9de1",
-  storageBucket: "wedding-website-2026-a9de1.firebasestorage.app",
-  messagingSenderId: "615864428692",
-  appId: "1:615864428692:web:25f7cdd70581a84851ec81",
-  measurementId: "G-269M8EM4JY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBl5sWc_QP5lsmLkYBmxeFyyY1NtExgw14",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "wedding-website-2026-a9de1.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "wedding-website-2026-a9de1",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "wedding-website-2026-a9de1.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "615864428692",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:615864428692:web:25f7cdd70581a84851ec81",
 };
 
 // Initialize Firebase
