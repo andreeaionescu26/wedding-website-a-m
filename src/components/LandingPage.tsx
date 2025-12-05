689b0c08577f47b030aea6f3a63ca644cba5edcc
andreeaionescu26 <andreea26.ionescu@gmail.com>
2025-12-05 16:38:41 +0000

    changed styling landing page -save the date

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -343,11 +343,11 @@ const LandingPage = ({ setView, setShowBurgerMenu }) => {
           {/* Header Section */}
           <div className="text-center mt-1 mb-12">
             {/* Animated Leaf */}
-            <div className={`mb-6 flex justify-center scale-75 md:scale-90 fade-header ${isVisible ? 'visible' : ''}`}>
+            <div className={` flex justify-center scale-75 md:scale-90 fade-header ${isVisible ? 'visible' : ''}`}>
               <LeafAnimation size="large" variant="horizontal"/>
             </div>
             
-            <p className={`text-sage-700 font-normal text-3xl md:text-4xl mb-4 fade-content ${isVisible ? 'visible' : ''}`}>
+            <p className={`text-sage-700 font-medium text-3xl md:text-4xl mb-4 fade-content ${isVisible ? 'visible' : ''}`}>
               {t.landing.saveDate}
             </p>
             
